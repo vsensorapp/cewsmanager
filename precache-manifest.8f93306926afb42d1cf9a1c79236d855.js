2138b7b4570f5cb9eeac02e6578a7486033eaaf4
vsensorapp <43778115+vsensorapp@users.noreply.github.com>
2019-02-17 22:34:29 +0800

    Add files via upload

---
+++ precache-manifest.8f93306926afb42d1cf9a1c79236d855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "132f3f9dee6cdb61376e",
+    "url": "/js/chunk-vendors.0019c92b.js"
+  },
+  {
+    "revision": "607fee4f580cfd60d4ea",
+    "url": "/js/app.42cb13a2.js"
+  },
+  {
+    "revision": "af5dfe49b8c5c7f4c879ed697e809781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "132f3f9dee6cdb61376e",
+    "url": "/css/chunk-vendors.60015a9f.css"
+  },
+  {
+    "revision": "607fee4f580cfd60d4ea",
+    "url": "/css/app.8ec2d520.css"
+  }
+];
